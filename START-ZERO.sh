6760f5896d2799917763b8754673d8b35693d3f8
oussamabalbal <46396011+oussamabalbal1@users.noreply.github.com>
2025-01-26 11:44:39 -0500

    Update START-ZERO.sh

--- START-ZERO.sh
+++ START-ZERO.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+
+# Define the countdown function
+countdown() {
+    local duration=$1
+
+    # Start time
+    local start_time=$(date +%s)
+
+    # End time
+    local end_time=$((start_time + duration))
+
+    echo "Sleeping for $duration seconds..."
+
+    while [ $(date +%s) -lt $end_time ]; do
+        local remaining_time=$((end_time - $(date +%s)))
+        echo -ne "Time remaining: $remaining_time seconds\033[0K\r"
+        sleep 1
+    done
+
+    echo -e "\nDone sleeping!"
+}
+
+# Example usage of the function
+# countdown 10  # Sleep for 10 seconds
+
+
 # Define variables
 GIT_REPO_URL="https://github.com/oussamabalbal1/project-zero"
 PROJECT_DIR="/home/ubuntu/project-zero"
@@ -28,7 +54,7 @@ if [[ -d "$PLAYBOOKS_DIR" ]]; then
   echo "Will execute playbooks that inside $PLAYBOOKS_DIR..."
 
   # List files, sort numerically and alphabetically, and store in a variable
-  PLAYBOOKS=($(ls -1 "$PLAYBOOKS_DIR" | grep -i yaml | sort -V))
+  PLAYBOOKS=($(ls -1 "$PLAYBOOKS_DIR" | grep -i yaml | sort -V | head -10))
 
   # Print the sorted files
   echo "Execute playbooks in ordred way like this :"
@@ -43,7 +69,8 @@ fi
 for playbook in "${PLAYBOOKS[@]}"; do
     echo "Running $playbook..."
     if [[ "$playbook" == "5-k8s-install-dependencies.yaml" ]]; then
-        sleep 4m
+        # sleep 4m
+        countdown 240  # Sleep for 10 seconds
         ansible-playbook "$playbook" -i "$INVENTORY_FILE"
     else
         ansible-playbook "$playbook" -i "$INVENTORY_FILE"
@@ -53,3 +80,5 @@ for playbook in "${PLAYBOOKS[@]}"; do
 done
 
 echo "All tasks completed."
+
+
